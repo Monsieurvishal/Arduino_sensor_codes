dd3bbfa34cd8e793034bf41cf6984dd6e92988f1
Vishal Naik <59202862+Monsieurvishal@users.noreply.github.com>
2020-04-05 22:07:48 +0530

    Create touch sensor.ino

---
+++ touch sensor.ino
@@ -0,0 +1,26 @@
+//TTP223-IC
+#define sensorPin 1 // capactitive touch sensor - Arduino Digital pin D1
+ 
+int ledPin = 13; // Output display LED (on board LED) - Arduino Digital pin D13
+ 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);  
+  pinMode(sensorPin, INPUT);
+}
+ 
+void loop() 
+{
+  int senseValue = digitalRead(sensorPin);
+  if (senseValue == HIGH){
+    digitalWrite(ledPin, HIGH);
+    Serial.println("TOUCHED");
+  }
+  else{
+    digitalWrite(ledPin,LOW);
+    Serial.println("not touched");
+  } 
+  delay(500);
+  
+}
