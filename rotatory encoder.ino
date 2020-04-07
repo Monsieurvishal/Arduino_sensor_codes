07f694dae2b33eb69799a5a2c866cb8d8cea4a7a
Vishal Naik <59202862+Monsieurvishal@users.noreply.github.com>
2020-04-07 17:25:33 +0530

    Update rotatory encoder.ino

--- rotatory encoder.ino
+++ rotatory encoder.ino
@@ -3,7 +3,8 @@
  int counter = 0; 
  int aState;
  int aLastState;  
- void setup() { 
+ void setup()
+ { 
    pinMode (outputA,INPUT);
    pinMode (outputB,INPUT);
    
@@ -11,10 +12,12 @@
    // Reads the initial state of the outputA
    aLastState = digitalRead(outputA);   
  } 
- void loop() { 
+ void loop()
+ { 
    aState = digitalRead(outputA); // Reads the "current" state of the outputA
    // If the previous and the current state of the outputA are different, that means a Pulse has occured
-   if (aState != aLastState){     
+   if (aState != aLastState)
+   {     
      // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
      if (digitalRead(outputB) != aState) { 
        counter ++;
@@ -23,6 +26,13 @@
      }
      Serial.print("Position: ");
      Serial.println(counter);
-   } 
+    if(counter>=30)
+    {
+     counter=0;
+    
+    }
+   }
+  
+  
    aLastState = aState; // Updates the previous state of the outputA with the current state
 }
